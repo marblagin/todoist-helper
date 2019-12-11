b4593d36b3237d27b043b726753698d3f50c62ac
Llewellyn CRaddock <lacraddock@gmail.com>
2019-12-11 16:42:29 +0000

    Fixed and working

--- todoist-helper.py
+++ todoist-helper.py
@@ -2,8 +2,69 @@ import sys
 from todoist.api import TodoistAPI
 
 
-def print_projects(api_object):
-    return api_object.state['projects']
+class Title:
+    def __init__(self):
+        self.company_name = input("Enter in company name: ")
+        self.isr = input("Enter in ISR name: ")
+        self.products = self.validate_products()
+
+    @staticmethod
+    def validate_products():
+        while True:
+            try:
+                prod = input("Enter in a product: ").split()
+                for x in range(len(prod)):
+                    if prod[x] == "CBD" or prod[x] == "CBLO" or prod[x] == "CBTH" \
+                            or prod[x] == "CBR" or prod[x] == "CBP":
+                        return prod
+                print("Please enter in a valid product")
+            except ValueError:
+                print("Please enter in a valid product")
+
+    def get_title(self):
+        prods = ""
+        for x in range(len(self.products)):
+            prods = prods + self.products[x] + " "
+        return str(self.company_name + " - " + self.isr + " - " + prods)
+
+    def get_products(self):
+        return self.products
+
+
+def print_projects(api_object, verbose=False):
+    if verbose:
+        print(api_object.state['projects'])
+    else:
+        for x in range(len(api_object.state['projects'])):
+            print(api_object.state['projects'][x]['name'])
+
+
+def print_items(api_object, project_id, verbose=False):
+    if verbose:
+        for y in range(len(api_object.state['items'])):
+            if api_object.state['items'][y]['project_id'] == project_id:
+                print(api_object.state['items'][y])
+    else:
+        for y in range(len(api_object.state['items'])):
+            if api_object.state['items'][y]['project_id'] == project_id:
+                print(api_object.state['items'][y]['content'])
+
+
+def assign_company_name(api_object, project_id, template_name, title, company_name):
+    parent_id = ""
+    for y in range(len(api_object.state['items'])):
+        if api_object.state['items'][y]['content'] == template_name and \
+                api_object.state['items'][y]['project_id'] == project_id:
+            parent_id = api_object.state['items'][y]['id']
+            parent_item = api_object.items.get_by_id(parent_id)
+            parent_item.update(content=title)
+    for y in range(len(api_object.state['items'])):
+        if api_object.state['items'][y]['parent_id'] == parent_id:
+            original_content = api_object.state['items'][y]['content']
+            item = api_object.items.get_by_id(api_object.state['items'][y]['id'])
+            new_content = company_name + " - " + original_content
+            item.update(content=new_content)
+    api_object.commit()
 
 
 def import_csv(api_object, project_id, csv):
@@ -16,21 +77,38 @@ def export_to_url(api_object, project_id):
 
 def get_project_id(data):
     if data == "new demo":
+        # Demo Project ID
         return 2213838333
     else:
+        # POC Project ID
         return 2215732027
 
 
 def get_csv(data, product):
+
     if data == "new demo":
         return 'templates/Demo_Checklist.csv'
-    elif product == "cbd":
-        print("Returning CBD")
-        return 'templates/POC_CBC.csv'
-    elif product == "cbr":
-        return 'templates/POC_CBR.csv'
-    elif product == "cbp":
-        return 'templates/POC_CBP.csv'
+    else:
+        for x in range(len(product)):
+            if product[x] == "CBD" or product[x] == "CBLO" or product[x] == "CBTH":
+                return 'templates/POC_CBC.csv'
+            elif product[x] == "CBR":
+                return 'templates/POC_CBR.csv'
+            elif product[x] == "CBP":
+                return 'templates/POC_CBP.csv'
+
+
+def get_template_name(products, command):
+    if command == "new demo":
+        return 'Demo Checklist Template:'
+    else:
+        for x in range(len(products)):
+            if products[x] == "CBD" or products[x] == "CBLO" or products[x] == "CBTH":
+                return 'CBC POC Checklist Template:'
+            elif products[x] == "CBR":
+                return 'CBR POC Checklist Template:'
+            elif products[x] == "CBP":
+                return 'CBP POC Checklist Template:'
 
 
 def validate_command():
@@ -38,35 +116,26 @@ def validate_command():
         try:
             com = input("Enter in a command: ")
             if com == "new demo" or com == "new poc":
-                break
+                return com
             else:
                 print("Please enter in a valid command")
         except ValueError:
             print("Please enter in a valid command")
-    return com
-
-
-def validate_product():
-    while True:
-        try:
-            prod = input("Enter in a product: ")
-            if prod == "cbd" or prod == "cbr" or prod == "cbp":
-                break
-            else:
-                print("Please enter in a valid product")
-        except ValueError:
-            print("Please enter in a valid product")
-    return prod
 
 
 def main():
     api = TodoistAPI('1bfa68a130d0206b525c94f22e524f4564b4f0ad')
     api.sync()
     command = validate_command()
-    product = validate_product()
+    title = Title()
+    products = title.get_products()
     project_id = get_project_id(command)
-    csv = get_csv(command, product)
+    csv = get_csv(command, products)
+    template_name = get_template_name(products, command)
     import_csv(api, project_id, csv)
+    api.sync()
+    assign_company_name(api, project_id, template_name, title.get_title(), title.company_name)
+    api.sync()
 
 
 if __name__ == "__main__":
